6705a926b54f2a88d36db3693ccf02bb13af5c07
Diego Benzonkar <82963508+benzonnigggon@users.noreply.github.com>
2023-04-04 08:20:28 +0545

    lil change

---
+++ js/custom.js
@@ -0,0 +1,35 @@
+$(window).on("load resize", function() {
+
+    var counters = $(".count");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    for (i = 0; i < countersQuantity; i++) {
+        counter[i] = parseInt(counters[i].innerHTML);
+    }
+
+    var count = function(start, value, id) {
+        var localStart = start;
+        setInterval(function() {
+            if (localStart < value) {
+                localStart++;
+                counters[id].innerHTML = localStart;
+            }
+        }, 15);
+    };
+
+    for (j = 0; j < countersQuantity; j++) {
+        count(0, counter[j], j);
+    }
+});
+
+$(document).ready(function() {
+
+    $('#scrollBottom').click(function() {
+        $('html, body').animate({
+            scrollTop: $(document).height()
+        }, 'slow');
+        return false;
+    });
+
+});
